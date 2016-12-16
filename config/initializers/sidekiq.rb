28a3778b7ddd2de89f31e095b12bb922379aa4a2
Denis Isakov <denisakov@gmail.com>
2016-12-16 02:27:41 +0200

    starting sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDIS_URL'], size: 2 }
+    config.redis = { url: ENV['REDIS_URL'], size: 1 }
   end
 
   Sidekiq.configure_server do |config|
@@ -13,7 +13,7 @@ if Rails.env.production?
 
       ActiveSupport.on_load(:active_record) do
         config = Rails.application.config.database_configuration[Rails.env]
-        config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
+        #config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
         config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
         #config['pool'] = 16
         ActiveRecord::Base.establish_connection(config)
